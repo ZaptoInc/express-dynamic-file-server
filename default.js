666ead390312ed1533046c41fb84c00b4aee2040
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2023-10-08 00:01:00 +0200

    added onlineFolder (acts like a web ftp server)

--- default.js
+++ default.js
@@ -5,6 +5,7 @@ module.exports = {
         password:"", //password as bcrypt
         headers : [], //list of headers added to the result
         allowedHostname : [], //list of hostnames allowed to use this path
-        folder : null
+        folder : null, //folder to use, or null to use the default one (with relative path)
+        onlineFolder : false //acts as an online folder with navigation and folder/file listing
     }
 }
